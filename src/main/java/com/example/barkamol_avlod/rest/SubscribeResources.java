2e4aba0692757f8a63c82a76fbbdd3866b8fd84f
karimjon2901 <xakimjonovkarimjon37@gmail.com>
2023-11-19 09:20:34 +0500

    Subscribe controller edited

--- src/main/java/com/example/barkamol_avlod/rest/SubscribeResources.java
+++ src/main/java/com/example/barkamol_avlod/rest/SubscribeResources.java
@@ -3,6 +3,8 @@ package com.example.barkamol_avlod.rest;
 import com.example.barkamol_avlod.dto.ResponseDto;
 import com.example.barkamol_avlod.dto.SubscribeDto;
 import com.example.barkamol_avlod.service.SubscribeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,12 +18,25 @@ import java.util.List;
 public class SubscribeResources {
     private final SubscribeService service;
 
-
+    @Operation(
+            method = "Add statistics",
+            description = "Need to send SubscribeDto to this endpoint to add",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Subscribe info",
+                    content = @Content(mediaType = "application/json")),
+            summary = "Add"
+    )
     @PostMapping
     public ResponseDto<SubscribeDto> add(@Valid @RequestBody SubscribeDto subscribeDto){
         return service.add(subscribeDto);
     }
 
+    @Operation(
+            method = "Get all subscribes",
+            description = "This endpoint return all subscribeDto",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Subscribe info",
+                    content = @Content(mediaType = "application/json")),
+            summary = "Get all"
+    )
     @SecurityRequirement(name = "Authorization")
     @GetMapping
     public ResponseDto<List<SubscribeDto>> getAll(){
